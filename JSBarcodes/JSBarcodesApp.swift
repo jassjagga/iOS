ff6d2b04f1dc1abc16c76342a2165ec96166a57f
Jaswinder Singh <jaswindersinghjagga@gmail.com>
2024-11-06 02:08:45 -0500

    Initial Commit

---
+++ JSBarcodes/JSBarcodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSBarcodesApp.swift
+//  JSBarcodes
+//
+//  Created by Jaswinder SINGH on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct JSBarcodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JSBarcodesTests/JSBarcodesTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSBarcodesTests.swift
+//  JSBarcodesTests
+//
+//  Created by Jaswinder SINGH on 2024-11-06.
+//
+
+import Testing
+@testable import JSBarcodes
+
+struct JSBarcodesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
